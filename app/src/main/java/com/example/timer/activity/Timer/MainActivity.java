6120576f4fdfc6c0b7a3d4bf69268b0d27285881
LeeJaeYun <leejaeyun95@naver.com>
2020-06-11 11:02:00 +0900

    3st file

--- app/src/main/java/com/example/timer/activity/Timer/MainActivity.java
+++ app/src/main/java/com/example/timer/activity/Timer/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.timer.activity.Timer;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.timer.R;
 
@@ -18,11 +21,16 @@ public class MainActivity extends AppCompatActivity {
 
     private Button btnStart;
     private Button btnReset;
+    private Button btnOpen;
+    private Button btnClose;
     private EditText setTime;
     private EditText leftTime;
+    private CheckBox lockDrawer;
+
 
     private Timer timer;
     private TimerTask timerTask;
+    private DrawerLayout drawerLayout;
     private int settime;
     private int counter = 0;
 
@@ -30,7 +38,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //버튼 찾아라---------------------------------------------------------------//
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        btnStart = (Button) findViewById(R.id.btnStart);
+        btnReset = (Button) findViewById(R.id.btnReset);
+        btnOpen = findViewById(R.id.openDrawer);
+        btnClose = findViewById(R.id.closeDrawer);
+        lockDrawer = findViewById(R.id.lockDrawer);
+        //-------------------------------------------------------------------------//
         //타이머 Task 설정----------------------------------------------------------//
         timerTask = new TimerTask() {
             @Override
@@ -38,15 +53,14 @@ public class MainActivity extends AppCompatActivity {
                 Log.e("Left Time : ", String.valueOf(counter));
                 leftTime.setText(String.valueOf(counter), TextView.BufferType.NORMAL);
                 counter--;
-                if (counter == 0)
-                {
+                if (counter == 0) {
                     counter = settime;
                 }
             }
         };
-        //끝 타이머 Task 설정-------------------------------------------------------//
+        //------------------------------------------------------------------------//
         //타이머 주기 설정----------------------------------------------------------//
-        btnStart = (Button) findViewById(R.id.btnStart);
+
         btnStart.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -59,9 +73,8 @@ public class MainActivity extends AppCompatActivity {
                 timer.schedule(timerTask, 1000, 1000);
             }
         });
-        //끝 타이머 주기 설정-------------------------------------------------------//
+        //------------------------------------------------------------------------//
         //타이머 종료--------------------------------------------------------------//
-        btnReset = (Button) findViewById(R.id.btnReset);
         btnReset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -69,7 +82,44 @@ public class MainActivity extends AppCompatActivity {
                 counter = settime;
             }
         });
-        //끝 타이머 종료------------------------------------------------------------//
+        //------------------------------------------------------------------------//
+        //드로우 레이아웃 Open------------------------------------------------------//
+        btnOpen.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!drawerLayout.isDrawerOpen(Gravity.LEFT)) {
+                    drawerLayout.openDrawer(Gravity.LEFT); // 중력이 왼쪽에 있으면 왼쪽으로 잡아 당기는 건가??
+                }
+            }
+        });
+        //------------------------------------------------------------------------//
+        //드로우 레이아웃 Close------------------------------------------------------//
+        btnClose.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (drawerLayout.isDrawerOpen(Gravity.LEFT)) {
+                    drawerLayout.closeDrawer(Gravity.LEFT);
+                }
+            }
+        });
+        //------------------------------------------------------------------------//
+        //드로우 레이아웃 움직임 Lock------------------------------------------------//
+        lockDrawer.setOnClickListener(new CheckBox.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (lockDrawer.isChecked()) {  // 원문 내용과 다르게 작성함! 오류 나도 몰라!!
+                    if (drawerLayout.isDrawerOpen(Gravity.LEFT)) {
+                        drawerLayout.setDrawerLockMode(drawerLayout.LOCK_MODE_LOCKED_OPEN);
+                    } else {
+                        drawerLayout.setDrawerLockMode(drawerLayout.LOCK_MODE_LOCKED_CLOSED);
+                    }       // 드로우 레이아웃이 열렸을 때만 잠그는가 보다
+                } else {
+                        drawerLayout.setDrawerLockMode(drawerLayout.LOCK_MODE_UNLOCKED);
+                }
+            }
+        });
+        //------------------------------------------------------------------------//
+
     }
 
 }
